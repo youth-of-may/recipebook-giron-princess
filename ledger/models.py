e6b313b9009a3fe8cb65effebb33f78ba863b35a
Princess May Giron <135508335+youth-of-may@users.noreply.github.com>
2025-03-24 20:07:06 +0800

    Update models.py not to allow empty/null images for RecipeImage.

--- ledger/models.py
+++ ledger/models.py
@@ -36,7 +36,7 @@ class RecipeIngredient(models.Model):
 
 
 class RecipeImage(models.Model):
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(null=False, upload_to="images/")
     description = models.TextField(max_length=255)
     recipe = models.ForeignKey(
         Recipe, on_delete=models.CASCADE, related_name="recipe_image"
