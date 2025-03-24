4f3db4eef808cf7946da1af2c5f9cb48b635ebf9
youth-of-may <princess.giron@obf.ateneo.edu>
2025-03-24 15:33:21 +0800

    Edited urls.py to account for new methods

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('recipes/list', views.show_recipes, name='recipes'),
-    path('recipes/<int:pk>/', views.show_ingredients, name='recipe-detail'),
+    path("recipes/list", views.show_recipes, name="recipes"),
+    path("recipe/<int:pk>/", views.show_ingredients, name="recipe-detail"),
+    path("recipe/<int:pk>/add_image", views.add_recipe_image, name="recipe-add-image"),
+    path("recipe/add/", views.add_recipes, name="recipe-add"),
 ]
