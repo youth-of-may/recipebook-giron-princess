14c171c7be329061364466ea52bc14d792a39a3a
youth-of-may <princess.giron@obf.ateneo.edu>
2025-03-24 15:36:46 +0800

    Edited other files

--- recipebook/urls.py
+++ recipebook/urls.py
@@ -1,53 +1,32 @@
 """
-URL configuration for recipebook project.
+URL configuration for the recipebook project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to views. For more information, see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
+
 Examples:
-Function views
+Function views:
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
+    2. Add a URL to urlpatterns:  path("", views.home, name="home")
+
+Class-based views:
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+    2. Add a URL to urlpatterns:  path("", Home.as_view(), name="home")
+
+Including another URLconf:
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
 """
 
 from django.contrib import admin
 from django.urls import path, include
-from ledger.views import (
-    CustomView,
-    CustomPasswordReset,
-    CustomPasswordResetDone,
-    CustomPasswordResetConfirm,
-)
-from django.contrib.auth.views import (
-    LogoutView,
-    PasswordResetCompleteView,
-)
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("ledger.urls")),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts/login", CustomView.as_view(), name="login"),
-    path("accounts/logout", LogoutView.as_view(next_page="login"), name="logout"),
-    path("accounts/password_reset", CustomPasswordReset.as_view(), name="password_reset"),
-    path(
-        "accounts/reset/<uidb64>/<token>/",
-        CustomPasswordResetConfirm.as_view(),
-        name="password_reset_confirm",
-    ),
-    path(
-        "accounts/password_reset/done",
-        CustomPasswordResetDone.as_view(),
-        name="password_reset_done",
-    ),
-    path(
-        "accounts/reset/complete",
-        PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"),
-        name="password_reset_complete",
-    ),
+    path("", include("accounts.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
